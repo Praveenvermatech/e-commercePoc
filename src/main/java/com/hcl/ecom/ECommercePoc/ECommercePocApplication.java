b5528609a993536b9b9309e8d18d32e6bfaa16b7
Praveen Verma <praveen.verma@hcl.com>
2020-02-19 16:41:01 +0530

    Initial commit

---
+++ src/main/java/com/hcl/ecom/ECommercePoc/ECommercePocApplication.java
@@ -0,0 +1,14 @@
+package com.hcl.ecom.ECommercePoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommercePocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommercePocApplication.class, args);
+	System.out.println("Hello");
+	}
+
+}
